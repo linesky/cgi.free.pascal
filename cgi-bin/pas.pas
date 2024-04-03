5491ec45280a8262d15263d2f2ea6c89e11ed626
sky line <32512284+linesky@users.noreply.github.com>
2024-04-04 00:19:21 +0100

    Add files via upload

---
+++ cgi-bin/pas.pas
@@ -0,0 +1,36 @@
+{ Programm:  loto.pas
+   }
+
+
+program loto;
+
+
+
+var 
+  n: Integer;
+  a: Integer;
+  btn: Integer;
+  s1:String;
+     s2:String;
+begin
+
+if 0>=0 then 
+ begin 
+  
+s1:='[0][';
+for a := 0  to 8 do
+begin
+n:=random(49)+1;
+Str(n,s2);
+s1:=s1 + s2;
+s1:=s1 +'  '
+   end;
+ s1:=Concat(s1 , '.][ Ok ]');
+ writeln(s1); 
+ 
+ 
+    
+    
+end 
+
+end.
